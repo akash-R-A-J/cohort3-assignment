f60d80908351602472cff7695035f527a3e388be
Akash Raj <rajakash3852@gmail.com>
2024-08-04 21:06:30 +0530

    all assign of js-syntax of js is done.

--- js_practice/js_101_class.js
+++ js_practice/js_101_class.js
@@ -19,3 +19,28 @@ function canVote(age){
 
 console.log(canVote(15)); // 15 <= 18
 console.log(canVote(30)); // 30 > 18
+
+// if-else assignment
+function evenOrOdd(n){
+    if(n%2 == 0){
+        console.log('The number is even.');
+    }else{
+        console.log('The number is odd.');
+    }
+}
+
+evenOrOdd(5);
+evenOrOdd(8);
+
+// function assignment
+function sumUptoN(n){
+    let sum = 0;
+    while(n>0){
+        sum += n;
+        n--;
+    }
+
+    return sum;
+}
+
+console.log(sumUptoN(5));
