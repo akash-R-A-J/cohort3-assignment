017e6b72480de65c8795df62e208e52aed73b80f
Akash Raj <rajakash3852@gmail.com>
2024-08-08 11:10:39 +0530

    some modules are added in express-backend-app

---
+++ js_practice/express-backend-app/dashboard.js
@@ -0,0 +1,8 @@
+document.getElementById('loginBtn').addEventListener('click', function() {
+    window.location.href = 'login.html';
+  });
+  
+  document.getElementById('signupBtn').addEventListener('click', function() {
+    window.location.href = 'signup.html';
+  });
+  
