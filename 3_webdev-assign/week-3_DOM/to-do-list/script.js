2998f675ef9d5c640026b8a14e04fdcf1b5db782
Akash Raj <rajakash3852@gmail.com>
2024-11-05 02:58:50 +0530

    delete functionality added

--- 3_webdev-assign/week-3_DOM/to-do-list/script.js
+++ 3_webdev-assign/week-3_DOM/to-do-list/script.js
@@ -2,26 +2,53 @@ function get_todo() {
   // used to get the text from input field
   const input = document.querySelector("input"); // input variable is referenced to input field
   let value = input.value.trim();
-  console.log(value);
   if (value === "") showAlert("Please enter your to-do.");
   else createStr(value);
 }
 
 function createStr(input) {
   // used to create the string of the to-do, using count and value
-  const arr = document.querySelectorAll("h4");
+  const arr = document.getElementsByClassName("todos");
   let str = "";
+  let count = 1;
+
   if (arr.length == 0) str = "1. " + input;
   // before arr, + is used to convert str to num
-  else str = +arr[arr.length - 1].innerHTML.split(".")[0] + 1 + ". " + input;
-  addElm(str);
+  else {
+    count = +arr[arr.length - 1][0].innerHTML.split(".")[0] + 1;
+    str = count + ". " + input;
+  }
+
+  addElm(str, count);
 }
 
-function addElm(value) {
+function addElm(value, count) {
   // used to add the given to-do in the list
-  let h4 = document.createElement("h4");
-  h4.innerHTML = value; // or h4.appendChild(value)
-  document.getElementById("todos").appendChild(h4);
+  let todos = document.createElement("div");
+  todos.id = "todos-" + count;
+  todos.classList.add("todos");
+  todos.dataset.index = count; // dataset.index is used to indexing the todos for deleting purpose
+
+  let elm = document.createElement("div");
+  elm.innerHTML = value; // or h4.appendChild(value)
+  todos.appendChild(elm);
+
+  let btn = document.createElement("button");
+  btn.classList.add("todos-btn");
+  btn.textContent = "Delete";
+  btn.addEventListener("click", function () {
+    deleteTodo(count)
+  });
+
+  todos.appendChild(btn);
+  document.getElementById("todos").appendChild(todos);
+}
+
+function deleteTodo(index){
+  const toDelete = document.querySelector(`div[data-index='${index}']`);
+  if (toDelete) {
+    toDelete.remove(); // Remove the div from the DOM
+  }
 }
 
 // Function to show the custom alert
