f9a797d65ccc513b2a2d1c50b1298033eb89731d
Akash Raj <rajakash3852@gmail.com>
2025-08-13 19:56:21 +0530

    bootstrapped brainly

---
+++ 3_webdev-assign/week-15-project/brainly/src/index.ts
@@ -0,0 +1,31 @@
+import express from "express";
+
+const app = express();
+
+app.post("/api/v1/signup", (req, res) =>{
+    
+})
+
+app.post("/api/v1/signin", (req, res) => {
+    
+})
+
+app.post("/api/v1/content", (req, res) =>{
+    
+})
+
+app.get("/api/v1/content", (req, res) =>{
+    
+})
+
+app.delete("/api/v1/content", (req, res) =>{
+    
+})
+
+app.post("/api/v1/brain/share", (req, res) =>{
+    
+})
+
+app.get("/api/v1/brain/:shareLink", (req, res) =>{
+    
+})
